0544cc02ba538714577dd33e05de450621cde0c4
willf123@yahoo.com <willf123@yahoo.com>
2018-06-10 15:50:23 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {
+  Prince: "1999",
+  Michael Jackson: "beat it",
+  Van Halen: "right now"
+};
+
+function updatePlaylist(obj, artist, song) {
+  
+  return 
+
+function removeFromPlayList(obj, artist){
+  return delete 
+}
